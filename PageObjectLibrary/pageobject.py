58a42150d685257ea14ed60e069a4b5cd8f36ea9
John Doe <john@sample.com>
2016-12-29 01:49:45 +0100

    Cleanup

--- PageObjectLibrary/pageobject.py
+++ PageObjectLibrary/pageobject.py
@@ -8,7 +8,7 @@ from locatormap import LocatorMap
 from robot.utils.connectioncache import ConnectionCache
 import ExtendedSelenium2Library
 
-# These two lines needs to be executed only once
+# These two lines needs to be executed only once and then the instance is always available in cache
 cache_sel2lib = ConnectionCache()
 cache_sel2lib.register (ExtendedSelenium2Library.ExtendedSelenium2Library(), alias='page_instance')
 
@@ -118,45 +118,4 @@ class PageObject():
         self.logger.info("expected title: '%s'" % expected_title)
         self.logger.info("  actual title: '%s'" % actual_title)
         raise Exception("expected title to be '%s' but it was '%s'" % (expected_title, actual_title))
-        return False
-
-class LoginPage(PageObject):
-    PAGE_TITLE = "Login - PageObjectLibrary Demo"
-    PAGE_URL = "/login.html"
-
-    # these are accessible via dot notaton with self.locator
-    # (eg: self.locator.username, etc)
-    _locators = {
-        "username": "id=id_username",
-        "password": "id=id_password",
-        "submit_button": "id=id_submit",
-    }
-
-    def create_browser(self, browser_name):
-        self.se2lib.create_webdriver(browser_name)
-    
-    def navigate_to(self, url):
-        self.se2lib.go_to(url)
-
-    def login_as_a_normal_user(self):
-        config = BuiltIn().get_variable_value("${CONFIG}")
-        self.enter_username(config.username)
-        self.enter_password(config.password)
-        with self._wait_for_page_refresh():
-            self.click_the_submit_button()
-
-    def enter_username(self, username):
-        """Enter the given string into the username field"""
-        self.se2lib.input_text(self.locator.username, username)
-
-    def enter_password(self,password):
-        """Enter the given string into the password field"""
-        self.se2lib.input_text(self.locator.password, password)
-
-    def click_the_submit_button(self):
-        """Click the submit button, and wait for the page to reload"""
-        with self._wait_for_page_refresh():
-            self.se2lib.click_button(self.locator.submit_button)
-            return HomePage()
-    
-    
+        return False    
