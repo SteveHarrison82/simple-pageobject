ee93eb15b23a462050a902babb323ddf6c311cb4
John Doe <john@sample.com>
2016-12-29 03:27:50 +0100

    version 2: using composition

---
+++ PageObjectLibrary/loginpage_v2_using_composition.py
@@ -0,0 +1,43 @@
+from pageobject import PageObject
+from homepage import HomePage
+from locatormap import LocatorMap
+
+class LoginPage():
+
+    PAGE_TITLE = "Login - PageObjectLibrary Demo"
+    PAGE_URL = "/login.html"
+
+    # these are accessible via dot notaton with self.locator
+    # (eg: self.locator.username, etc)
+
+
+    _locators = {
+        "username": "id=id_username",
+        "password": "id=id_password",
+        "submit_button": "id=id_submit",
+    }
+
+    def __init__(self):
+        self.po = PageObject()
+        self.se2lib = self.po.se2lib
+        self.locator = LocatorMap(getattr(self, "_locators", {}))
+
+    def create_browser(self, browser_name):
+        self.se2lib.create_webdriver(browser_name)
+    
+    def navigate_to(self, url):
+        self.se2lib.go_to(url)
+
+    def enter_username(self, username):
+        """Enter the given string into the username field"""
+        self.se2lib.input_text(self.locator.username, username)
+
+    def enter_password(self, password):
+        """Enter the given string into the password field"""
+        self.se2lib.input_text(self.locator.password, password)
+
+    def click_the_submit_button(self):
+        """Click the submit button, and wait for the page to reload"""
+        with self.po._wait_for_page_refresh():
+            self.se2lib.click_button(self.locator.submit_button)
+            return HomePage()
